6f82784f084621aaedd9857ee4814d49e59e6e0f
LiTingjun <739142578@qq.com>
2017-06-09 00:31:55 +0800

    exp3 header token 1

---
+++ exp3/exp3.y
@@ -0,0 +1,8 @@
+%{
+    #include <stdio.h>
+%}
+
+%token NUMBER
+%token ADD SUB MUL DIV  MOD
+%token OP CP
+%token EOL
--- exp3/test1.c
+++ exp3/test1.c
@@ -4,11 +4,27 @@
 int main(){
   int a, b, d;
   // Comment 2
-  char _as;
-  bool ce;
-  a = 1+3-9*(126/574);
+  char _as[100];
+  bool ce=false;
+  bool jko=!ce;
+  a = 1+3%2-9*(126/574);
   b = 5*9;
   d = 8/78;
   double e = 369 % 578;
+  e=1.25;
+  char h = 'c';
+  char m = '\'';
+  if(ce != false){
+    /*Do something*/
+    a=b-d;
+  }else{
+    switch(a){
+      case 0:
+        d=b;
+        break;
+      default:
+        e=25.5496;
+    }
+  }
   return 0;
 }
